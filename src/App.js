f916057f80af165dae1dc463ca88436f16ff1039
Kaiena <kaienafmartins@gmail.com>
2022-11-30 09:07:33 -0300

    add footer const

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <LogoName>ZapRecall</LogoName>
       </LogoContainer>
 
-
+      <Footer></Footer>
     </ScreenContainer>
     
   );
@@ -49,5 +49,21 @@ const LogoName = styled.h1 `
     margin-left: 20px;
 `
 
+const Footer = styled.div `
+  width: 100%;
+  min-height: 50px;
+  background-color: #FFFFFF;
+  position: fixed;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-family: 'Recursive';
+  font-weight: 400;
+  font-size: 18px;
+  color: #333333;
+  padding: 10px;
+`
 
 export default App;
