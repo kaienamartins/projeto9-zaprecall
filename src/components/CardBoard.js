4c283e40debe5a4f47184baafd9e5773497c044b
Kaiena <kaienafmartins@gmail.com>
2022-11-30 10:43:11 -0300

    add card deck

---
+++ src/components/CardBoard.js
@@ -0,0 +1,29 @@
+const cards = [
+  {  
+      id: 1,
+      pergunta: "O que é JSX?",
+      resposta: "Uma extensão de linguagem do JavaScript"
+  }, 
+
+  {
+      id: 2,
+      pergunta: "O React é:",
+      resposta: "Uma biblioteca JavaScript para construção de interfaces"
+  },
+
+  {
+      id: 3,
+      pergunta: "Componentes devem iniciar com:",
+      resposta: "Letra maiúscula"
+  },
+
+  {
+      id: 4,
+      pergunta: "Podemos colocar __ dentro do JSX",
+      resposta: "expressões"
+  },
+];
+
+
+
+export default cards;
