dde0aede92e1fddf6ee496f58744741608cec9dd
Kaiena <kaienafmartins@gmail.com>
2022-12-05 11:14:15 -0300

    create function to show question and answer

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,15 +4,44 @@ import RightIcon from "../assets/img/icone_certo.png";
 import AlmostIcon from "../assets/img/icone_quase.png";
 import TurnCard from "../assets/img/seta_virar.png";
 import styled from "styled-components";
+import { useState } from "react";
+import Buttons from "./Buttons";
 
-export default function Flashcard() {
+export default function Flashcard(props) {
+  const [status, setStatus] = useState({ cor: "#333333", icon: Play });
+  const [closed, setClosed] = useState(true);
+  const [openQuestion, setOpenQuestion] = useState(false);
+  const [openAnswer, setOpenAnswer] = useState(false);
+
+  function showQuestion() {
+    setClosed(false);
+    setOpenQuestion(true);
+  }
+
+  function showAnswer() {
+    setOpenQuestion(false);
+    setOpenAnswer(true);
+  }
+
+
+
   return (
     <>
-    
+      <Closed cor={status.cor} onClick={showQuestion}>
+        <p>Pergunta {props.dados.id}</p>
+        <img src={status.icon} alt="/" />
+      </Closed>
+      <Question>
+          <p>{props.dados.question}</p>
+          <img onClick={showAnswer} src={TurnCard} alt="/" />
+      </Question>
+      <Answer>
+        <p>{props.dados.answer}</p>
+        <Buttons/>
+      </Answer>
     </>
   );
 }
-
 
 const Closed = styled.div`
   width: 300px;
@@ -34,7 +63,6 @@ const Closed = styled.div`
     font-weight: 700;
     font-size: 16px;
     line-height: 19px;
-    
   }
 `;
 
@@ -85,4 +113,3 @@ const Answer = styled.div`
   flex-direction: column;
   justify-content: space-between;
 `;
-
