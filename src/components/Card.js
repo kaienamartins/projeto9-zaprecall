e36e1513cde38a0f71217ed124e48d4b5925d5ec
Kaiena <kaienafmartins@gmail.com>
2022-12-05 11:03:13 -0300

    create styled-components to cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,6 +7,99 @@ import TurnCard from "../assets/img/seta_virar.png";
 export default function Flashcard() {
   return (
     <>
+    
     </>
   );
 }
+
+
+const Closed = styled.div`
+  width: 300px;
+  height: 35px;
+  background-color: #ffffff;
+  margin: 12px;
+  padding: 15px;
+  box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  &:hover {
+    cursor: pointer;
+  }
+  p {
+    font-family: "Recursive", sans-serif;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 19px;
+    
+  }
+`;
+
+const Question = styled.div`
+  width: 300px;
+  margin: 12px;
+  padding: 15px;
+  min-height: 100px;
+  background: #ffffd5;
+  box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+  border-radius: 5px;
+  font-family: "Recursive";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 22px;
+  color: #333333;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  img {
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+    &:hover {
+      cursor: pointer;
+    }
+  }
+`;
+
+const Answer = styled.div`
+  width: 300px;
+  margin: 12px;
+  padding: 15px;
+  min-height: 100px;
+  background: #ffffd5;
+  box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+  border-radius: 5px;
+  font-family: "Recursive";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 22px;
+  color: #333333;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const BtnWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 5px;
+  border: none;
+`;
+
+const Btn = styled.button`
+  width: 85px;
+  height: 33px;
+  border-radius: 5px;
+  border: none;
+  font-family: "Recursive", sans-serif;
+  color: #ffffff;
+  &:hover {
+    cursor: pointer;
+  }
+`;
